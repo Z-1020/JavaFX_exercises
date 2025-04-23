b55abb0c2ecbcd990c515bd816dc1e4ccf1ade12
Zoe Margerie <margeri231@campus.unicaen.fr>
2025-04-23 16:27:32 +0200

    contenu des cases v1

--- tp3/src/main/java/iut/gon/tp3/GrilleController.java
+++ tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class GrilleController implements Initializable{
@@ -13,5 +15,15 @@ public class GrilleController implements Initializable{
 	@Override
 	public void initialize(URL url, ResourceBundle ressource) {
 		grille.setStyle("-fx-background-color: seashell");
+		Label tabLabel[][] = new Label[3][3];
+		for(int i =0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				tabLabel[i][j] =  new Label();
+				tabLabel[i][j].setText(String.format("L%dC%d", j, i));
+				tabLabel[i][j].onMouseClickedProperty();
+				grille.add(tabLabel[i][j], i, j);
+				
+			}
+		}
 	}
 }
