b058b66ef4ca5e20775d81fc7592f9704c8900d4
Zoe Margerie <margeri231@campus.unicaen.fr>
2025-05-01 21:14:22 +0200

    version final contenu des cases

--- tp3/src/main/java/iut/gon/tp3/GrilleController.java
+++ tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -3,11 +3,15 @@ package iut.gon.tp3;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 public class GrilleController implements Initializable{
 	private @FXML GridPane grille;
@@ -18,11 +22,16 @@ public class GrilleController implements Initializable{
 		Label tabLabel[][] = new Label[3][3];
 		for(int i =0; i<3; i++) {
 			for(int j=0; j<3; j++) {
-				tabLabel[i][j] =  new Label();
-				tabLabel[i][j].setText(String.format("L%dC%d", j, i));
-				tabLabel[i][j].onMouseClickedProperty();
-				grille.add(tabLabel[i][j], i, j);
-				
+				Label l = new Label();
+				l.setMaxSize(1000, 1000);
+				l.setAlignment(Pos.CENTER);
+				l.setTextAlignment(TextAlignment.CENTER);
+				l.setText(String.format("L%dC%d", j+1, i+1));
+				l.setOnMouseClicked(event -> {
+					l.setText("Bonjour");
+				});
+				 tabLabel[i][j] = l;
+				grille.add(l, i, j);
 			}
 		}
 	}
